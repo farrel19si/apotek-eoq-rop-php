dadf85874c1058fd736414a4e28b932d1651bd4d
farrel19si <farrel19si@mahasiswa.pcr.ac.id>
2023-05-23 20:29:16 +0700

    Mengubah query database Update stok

--- update_stok.php
+++ update_stok.php
@@ -3,7 +3,13 @@ include "koneksi.php";
 $id_barang = $_POST["id_barang"];
 $stok = $_POST["stok"];
 
-$query = "UPDATE barang SET stok ='$stok'  WHERE id_barang='$id_barang' ";
+if (isset($_POST['jumlah_tambah'])) {
+    $jumlah_tambah = $_POST['jumlah_tambah'];
+    // Lakukan sesuatu dengan nilai input yang diterima
+}
+
+
+$query = "UPDATE barang SET stok = stok + '$jumlah_tambah'  WHERE id_barang='$id_barang' ";
 
 
 if (mysqli_query($koneksi, $query)) {
